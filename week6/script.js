2d247c4363e0b04ecc6d501e6afbaf0327beb380
IMNOTJUSTIN778 <162271749+IMNOTJUSTIN778@users.noreply.github.com>
2024-04-30 15:21:54 +1000

    6

--- week6/script.js
+++ week6/script.js
@@ -1,34 +1,34 @@
-const myHeading = document.querySelector("h1")
+const myHeading = document.querySelectorAll("p");
 console.log(myHeading);
-//console.log(myHeading.textContent);
-//myHeading.textContent = "new paragraph";
-//myHeading.style.backgroundColor = "limegreen";
-//for (let i=0; i<3; i++)
-{
-    myHeading[i].textcontent ="new" + i;
-    myHeading[i].style.backgroundcolor = "limegreen"
+// console.log(myHeading.textContent);
+// myHeading.textContent = "new paragraph";
+// myHeading.style.backgroundColor = "limegreen";
+// for (let i = 0; i < 3; i++) {
+//   myHeading[i].textContent = "new" + i;
+//   myHeading[i].style.backgroundColor = "limegreen";
+// }
+
+
+// myHeading.forEach(changeMe);
+
+
+// function changeMe(item) {
+//   //   item.style.backgroundColor = "coral";
+//   item.classList.add("purple-box");
+// }
+
+
+const myButton = document.querySelector("#my-button");
+console.log(myButton);
+
+
+myButton.addEventListener("click", toggleMe);
+
+
+function toggleMe() {
+  const myImage = document.querySelector("#my-image");
+  console.log(myImage);
+  myImage.classList.toggle("round");
 }
 
-myHeading.foreach(changeMe);
 
-function changeMe(item){
-    //item.style.backgroundcolor = "coral"
-    item.classlist.add("purple-box");
-}
-
-const myimage = document.querySelector("#my-image");
-console.log(myimage);
-myimage.classList.add("round");
-
-const mybutton = document.querySelector("#my-button");
-console.log(mybutton);
-
-mybutton.addEventListener("click", toggleMe);
-
-function toggleMe()
-{
- const myimage = document.querySelector("#my-image");
-console.log(myimage);
-myimage.classList.add("round");
-
-}
