67d126af9e50207dbc06dfc22b68fb31c84f899d
IMNOTJUSTIN778 <162271749+IMNOTJUSTIN778@users.noreply.github.com>
2024-04-30 15:20:08 +1000

    6

---
+++ week6/script.js
@@ -0,0 +1,34 @@
+const myHeading = document.querySelector("h1")
+console.log(myHeading);
+//console.log(myHeading.textContent);
+//myHeading.textContent = "new paragraph";
+//myHeading.style.backgroundColor = "limegreen";
+//for (let i=0; i<3; i++)
+{
+    myHeading[i].textcontent ="new" + i;
+    myHeading[i].style.backgroundcolor = "limegreen"
+}
+
+myHeading.foreach(changeMe);
+
+function changeMe(item){
+    //item.style.backgroundcolor = "coral"
+    item.classlist.add("purple-box");
+}
+
+const myimage = document.querySelector("#my-image");
+console.log(myimage);
+myimage.classList.add("round");
+
+const mybutton = document.querySelector("#my-button");
+console.log(mybutton);
+
+mybutton.addEventListener("click", toggleMe);
+
+function toggleMe()
+{
+ const myimage = document.querySelector("#my-image");
+console.log(myimage);
+myimage.classList.add("round");
+
+}
