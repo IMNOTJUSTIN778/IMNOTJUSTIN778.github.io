bc52b6c09abba230accab000077430f70571fdad
IMNOTJUSTIN778 <162271749+IMNOTJUSTIN778@users.noreply.github.com>
2024-05-07 15:44:44 +1000

    7

--- week7/script.js
+++ week7/script.js
@@ -6,8 +6,29 @@ console.log(airportAudio);
 const playBUtton = document.querySelector("#play-button");
 console.log(playBUtton);
 
-playBUtton.addEventListener("click",playBUtton);
+playBUtton.addEventListener("click",playAirportAudio);
 
 function playAirportAudio(){
     airportAudio.play();
+}
+
+
+const pauseBUtton = document.querySelector("#pause-button");
+console.log(pauseBUtton);
+
+pauseBUtton.addEventListener("click", pauseAirportAudio);
+
+function pauseAirportAudio(){
+    airportAudio.pause();
+}
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+const popSound = document.querySelector("#pop-sound");
+
+popButton.addEventListener("click", popIt);
+
+function popIt(){
+    popSound.play();
 }
